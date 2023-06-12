1c05059c2e05f9ab521aee266e95110f850651f0
Atlantis357 <aaa26@illinois.edu>
2023-06-12 10:09:08 -0400

    Initial Commit

---
+++ Salam/SalamApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalamApp.swift
+//  Salam
+//
+//  Created by Abraham Alkhatib on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SalamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
